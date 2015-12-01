3f905d0ced3bf6382e5bedbf8cd97e26ebdb3180
tiger <flier.lh@gmail.com>
2015-12-01 09:31:00 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+_t=/tmp/$RANDOM; mkdir -v $_t || exit -1
+
+wget -O$_t -k https://raw.githubusercontent.com/netsnail/tools/master/install/_tomcat
+wget -O$_t -k https://raw.githubusercontent.com/netsnail/tools/master/install/tomcat
+wget -O$_t -k https://raw.githubusercontent.com/netsnail/tools/master/install/limits.conf
+wget -O$_t -k https://raw.githubusercontent.com/netsnail/tools/master/install/my.sh
+
+mkdir -pv /data/bin/
+
+cp -iv $_t/_tomcat 	/data/bin/
+cp -iv $_t/tomcat 	/data/bin/
+cp -iv $_t/limits.conf 	/etc/security/
+cp -iv $_t/my.sh 	/etc/profile.d/
+
+chmod 755 /etc/profile.d/my.sh
+chmod 644 /etc/security/limits.conf
+chmod 755 /data/bin/_tomcat
+chmod 755 /data/bin/tomcat
+
+rm -fv $_t/* && rmdir -v $_t
+
+echo "`whoami` ALL=(ALL) NOPASSWD: /data/bin/tomcat" >> /etc/sudoers
