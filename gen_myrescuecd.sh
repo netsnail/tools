988198ea74dc8be51e0f6c2786c5a02918ac8859
netsnail <flier.lh@gmail.com>
2016-05-27 10:49:16 +0800

    Update gen_myrescuecd.sh

--- gen_myrescuecd.sh
+++ gen_myrescuecd.sh
@@ -7,13 +7,14 @@ cd /root
 btrfs subvolume delete   -c       mygentoo_
 btrfs subvolume snapshot mygentoo mygentoo_
 
+# 创建initram.igz
 xz -d < initram.igz.origin |cpio -idm -D /tmp/myinitram
 sed -i 's@# 4@/sbin/modprobe -v aufs\n    # 4@' /tmp/myinitram/init
 cp -pr mygentoo/squashfs-root/lib/modules /tmp/myinitram/lib/
 ./gen_initramfs_list.sh -o initram.igz /tmp/myinitram
 rm -r /tmp/myinitram
 
+# 创建sysrcd.dat
 rm -r mygentoo_/squashfs-root/usr/portage/*
 rm -r mygentoo_/squashfs-root/usr/src/*
-
 mksquashfs mygentoo_/squashfs-root sysrcd.dat -noappend
