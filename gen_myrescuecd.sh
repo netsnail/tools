d15e13e57053111b6b50917b61c7d616542b3bae
netsnail <flier.lh@gmail.com>
2016-05-28 11:52:25 +0800

    Update gen_myrescuecd.sh

--- gen_myrescuecd.sh
+++ gen_myrescuecd.sh
@@ -21,3 +21,10 @@ cp -pr  mygentoo_/squashfs-root/usr/share/locale /tmp/__locale/
 rm -r   mygentoo_/squashfs-root/usr/share/locale/*
 mv      /tmp/__locale/locale.alias      mygentoo_/squashfs-root/usr/share/locale/
 mv      /tmp/__locale/zh_CN             mygentoo_/squashfs-root/usr/share/locale/
+echo >  mygentoo_/squashfs-root/root/.bash_history
+rm -r   mygentoo_/squashfs-root/usr/share/doc
+rm -r   mygentoo_/squashfs-root/usr/portage
+rm -r   mygentoo_/squashfs-root/usr/src
+
+# create sysrcd.dat
+mksquashfs mygentoo_/squashfs-root sysrcd.dat -noappend -comp xz
