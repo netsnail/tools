5d4b3d136b935c515b87ffc73c2964b8ce7d7a75
netsnail <flier.lh@gmail.com>
2016-05-30 19:23:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 _user="www"
 _jdk_url="http://172.16.96.119/jdk7.tar.xz"
 _tomcat_url="http://172.16.96.119/tomcat0.tar.xz"
@@ -8,10 +10,10 @@ _install_dir="/data"
 _t="/tmp/$RANDOM"; mkdir -v $_t || exit 1;
 
 cd $_t
-wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/_tomcat"
-wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/tomcat"
-wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/limits.conf"
-wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/my.sh"
+wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/_tomcat" 2>/dev/null
+wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/tomcat" 2>/dev/null
+wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/limits.conf" 2>/dev/null
+wget -k "https://raw.githubusercontent.com/netsnail/tools/master/install/my.sh" 2>/dev/null
 
 mkdir -pv $_install_dir/bin/ || exit 2;
 
@@ -28,12 +30,11 @@ chmod 755 $_install_dir/bin/tomcat
 rm -fv $_t/* && rmdir -v $_t
 
 echo "$_user ALL=(ALL) NOPASSWD: $_install_dir/bin/tomcat" >> /etc/sudoers
-wget $_jdk_url -O /tmp/jdk.tar.xz || { echo "downloading jdk failed!"; exit 3; }
+wget $_jdk_url -O /tmp/jdk.tar.xz 2>/dev/null || { echo "downloading jdk failed!"; exit 3; }
 tar -xf /tmp/jdk.tar.xz -C $_install_dir
-wget $_tomcat_url -O /tmp/tomcat.tar.xz || { echo "downloading tomcat failed!"; exit 4; }
+wget $_tomcat_url -O /tmp/tomcat.tar.xz 2>/dev/null || { echo "downloading tomcat failed!"; exit 4; }
 tar -xf /tmp/tomcat.tar.xz -C $_install_dir
 chown -R $_user $_install_dir/tomcat* && chown -R www-data $_install_dir/tomcat*/{logs,work,temp}
-
 mkdir $_install_dir/www && chown $_user $_install_dir/www
 
 echo -e "\ncomplete :)"
