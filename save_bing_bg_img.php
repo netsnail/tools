3d7f5655d36ef746d26c0a821c3a0093ad3cacb2
netsnail <flier.lh@gmail.com>
2016-05-31 06:15:43 -0500

    抓取bing搜索引擎的背景图片，用crontab每天抓一次

--- save_bing_bg_img.php
+++ save_bing_bg_img.php
@@ -1,11 +1,13 @@
 #!/usr/bin/php
 <?php
-$htmlfile = "index.html";
 $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 $array = json_decode($str);
 $imgurl = $array->{"images"}[0]->{"url"};
 $content = file_get_contents($imgurl);
 $imgname = basename($imgurl);
 file_put_contents($imgname, $content);
+
+// 生成index.html文件
+$htmlfile = "index.html";
 file_put_contents($htmlfile, "<img src=$imgname>\n".file_get_contents($htmlfile));
 ?>
