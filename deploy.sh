4a2197ee964dc0df49af4eefe325a63a766f42ac
netsnail <flier.lh@gmail.com>
2016-05-31 06:30:10 -0500

    nginx下tomcat中应用的备份升级部署

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 _p=xxxx ; cd /data/www ;
 
 echo '<meta charset="utf8"><meta http-equiv="refresh" content="5" /><center><br/><h3>请稍等，系统升级中...</h3>' >/data/nginx/html/upgrade.htm ;
-sleep 10 ; sudo /data/bin/tomcat 0 stop ;
+sleep 9 ; sudo /data/bin/tomcat 0 stop ;
 
 tar -jcf backup/$_p-$(date +'%Y%m%d%H%M').tar.bz2 $_p ;
 test -d /tmp/$_p && rm -rf /tmp/$_p ;
