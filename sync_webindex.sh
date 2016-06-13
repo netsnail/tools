75f61b2640e6f9573b1b5d699eeeba51a887a5cc
netsnail <flier.lh@gmail.com>
2016-06-13 08:36:08 -0500

    Update sync_webindex.sh

--- sync_webindex.sh
+++ sync_webindex.sh
@@ -5,7 +5,7 @@ _dir='/mnt/movies/'
 
 get_purl() {
   _n=1
-  curl $_url 2>/dev/null |sed -n 's/<a href="\(.*\)">\(.*\)<\/a>/\1 \2/p' |awk '{print $3"\t"$1}' |sort -t '-' -k3 -k2M -nk1 |awk '{print $2}' |while read i; do
+  curl -k $_url 2>/dev/null |sed -n 's/<a href="\(.*\)">\(.*\)<\/a>/\1 \2/p' |awk '{print $3"\t"$1}' |sort -t '-' -k3 -k2M -nk1 |awk '{print $2}' |while read i; do
     if [ $1 -eq $((_n++)) ]; then
       echo "$_url$i"
     fi
@@ -13,25 +13,25 @@ get_purl() {
 }
 
 list_durl() {
-  curl $1 2>/dev/null |grep -i -E ".mkv\"|.rmvb\"|.mp4\"" |sed -n 's/<a href="\(.*\)">[^:]*...\(.*\)/\1\2/p'
+  curl -k $1 2>/dev/null |grep -i -E ".mkv\"|.rmvb\"|.mp4\"" |sed -n 's/<a href="\(.*\)">[^:]*...\(.*\)/\1\2/p'
 }
 
 download() {
-  curl $1 2>/dev/null |grep -i -E ".mkv\"|.rmvb\"|.mp4\"" |sed -n 's/<a href="\(.*\)">.*/\1/p' |while read f; do
+  curl -k $1 2>/dev/null |grep -i -E ".mkv\"|.rmvb\"|.mp4\"" |sed -n 's/<a href="\(.*\)">.*/\1/p' |while read f; do
     _dfile="$1$f"
     echo downloading \"$_dfile\" ...
-    lftp -c "pget -O $_dir -n10 \"$_dfile\""
+    lftp -c "set ssl:verify-certificate no; pget -O $_dir -n10 \"$_dfile\""
   done
 }
 
 list() {
   _n=10 ; test -z "$1" || _n=$1
-  curl $_url 2>/dev/null |sed -n 's/<a href="\(.*\)">\(.*\)<\/a>/\1 \2/p' |awk '{print $3"\t"$1"\t"$2}' |sort -t '-'  -k3 -k2M -nk1 |awk '{print NR"\t"$3}' |tail -$_n
+  curl -k $_url 2>/dev/null |sed -n 's/<a href="\(.*\)">\(.*\)<\/a>/\1 \2/p' |awk '{print $3"\t"$1"\t"$2}' |sort -t '-'  -k3 -k2M -nk1 |awk '{print NR"\t"$3}' |tail -$_n
 }
 
 delete_purl() {
   _n=1
-  curl $_url 2>/dev/null |sed -n 's/<a href="\(.*\)">\(.*\)<\/a>/\1 \2/p' |awk '{print $3"\t"$1"\t"$2}' |sort -t '-' -k3 -k2M -nk1 |awk '{print $3}' |while read i; do
+  curl -k $_url 2>/dev/null |sed -n 's/<a href="\(.*\)">\(.*\)<\/a>/\1 \2/p' |awk '{print $3"\t"$1"\t"$2}' |sort -t '-' -k3 -k2M -nk1 |awk '{print $3}' |while read i; do
     if [ $1 -eq $((_n++)) ]; then
       _t="${i/%..&gt;/*}"
       _t="${_t//$/\\$}"
