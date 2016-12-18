f607ccc093afcb7c00596d0ae970b2dcda0860c3
netsnail <flier.lh@gmail.com>
2016-12-18 02:15:17 -0600

    我的12306

--- mytrain.sh
+++ mytrain.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 open() {
-_str=${1//\"/}
-#chrome "https://kyfw.12306.cn/otn/login/init"
-chrome "https://kyfw.12306.cn/otn/leftTicket/submitOrderRequest?tour_flag=dc&purpose_codes=ADULT&secretStr=$_str"
-chrome "https://kyfw.12306.cn/otn/confirmPassenger/initDc"
+  _str=${1//\"/}
+  #chrome "https://kyfw.12306.cn/otn/login/init"
+  chrome "https://kyfw.12306.cn/otn/leftTicket/submitOrderRequest?tour_flag=dc&purpose_codes=ADULT&secretStr=$_str"
+  chrome "https://kyfw.12306.cn/otn/confirmPassenger/initDc"
 }
 ok() {
-mpv ~/Musics/Aqua\ -\ Around\ The\ World.mp3
+  mpv ~/Musics/Aqua\ -\ Around\ The\ World.mp3
 }
 query() {
-_date=$1
-_from=$2
-_to=$3
-_num=$4
-res=$(curl -k -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36" "https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=$_date&leftTicketDTO.from_station=$_from&leftTicketDTO.to_station=$_to&purpose_codes=ADULT" 2>/dev/null | jq ".data[$_num]|{NO_:.queryLeftNewDTO.station_train_code,from:.queryLeftNewDTO.from_station_name,to:.queryLeftNewDTO.to_station_name,yw:.queryLeftNewDTO.yw_num,rw:.queryLeftNewDTO.rw_num,start_time:.queryLeftNewDTO.start_time,arrive_time:.queryLeftNewDTO.arrive_time,secretStr:.secretStr}")
-echo $res |jq '.'
-test -z "$res" && return
-echo $res |jq -e 'select((.yw != "无" and .yw != "--" and .yw !="null") or (.rw != "无" and .rw != "--" and .rw != "null"))'
-if test "$?" -eq "0"; then
-open $(echo $res |jq -e 'select((.yw != "无" and .yw != "--" and .yw !="null") or (.rw != "无" and .rw != "--" and .rw != "null")) |.secretStr')
-ok
-fi
-sleep .3
+  _date=$1
+  _from=$2
+  _to=$3
+  _num=$4
+  res=$(curl -k -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36" "https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=$_date&leftTicketDTO.from_station=$_from&leftTicketDTO.to_station=$_to&purpose_codes=ADULT" 2>/dev/null | jq ".data[$_num]|{NO_:.queryLeftNewDTO.station_train_code,from:.queryLeftNewDTO.from_station_name,to:.queryLeftNewDTO.to_station_name,yw:.queryLeftNewDTO.yw_num,rw:.queryLeftNewDTO.rw_num,start_time:.queryLeftNewDTO.start_time,arrive_time:.queryLeftNewDTO.arrive_time,secretStr:.secretStr}")
+  echo $res |jq '.'
+  test -z "$res" && return
+  _sstr=$(echo $res |jq -e 'select((.yw != "无" and .yw != "--" and .yw !="null") or (.rw != "无" and .rw != "--" and .rw != "null")) |.secretStr')
+  if test "$?" -eq "0"; then
+    open $_sstr
+    ok
+  fi
+  sleep .3
 }
 
 _date=2016-12-30
@@ -44,3 +44,5 @@ query $_date $_from BZH -1
 query $_date $_from FYH -1
 # k105,1303 菏泽
 query $_date $_from HIK "-2,-3"
+                                                                                                        
+                                                                     
