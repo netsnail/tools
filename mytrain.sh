733e010419b1a06d2446109e062578f78488ef41
netsnail <flier.lh@gmail.com>
2016-12-18 02:05:19 -0600

    我的火车票

--- mytrain.sh
+++ mytrain.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+open() {
+_str=${1//\"/}
+#chrome "https://kyfw.12306.cn/otn/login/init"
+chrome "https://kyfw.12306.cn/otn/leftTicket/submitOrderRequest?tour_flag=dc&purpose_codes=ADULT&secretStr=$_str"
+chrome "https://kyfw.12306.cn/otn/confirmPassenger/initDc"
+}
 ok() {
-/opt/google/chrome/chrome 'https://kyfw.12306.cn/otn/login/init'
 mpv ~/Musics/Aqua\ -\ Around\ The\ World.mp3
 }
-
 query() {
 _date=$1
 _from=$2
 _to=$3
 _num=$4
-res=$(curl -k -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36" "https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=$_date&leftTicketDTO.from_station=$_from&leftTicketDTO.to_station=$_to&purpose_codes=ADULT" 2>/dev/null | jq ".data[$_num]|{NO_:.queryLeftNewDTO.station_train_code,from:.queryLeftNewDTO.from_station_name,to:.queryLeftNewDTO.to_station_name,yw:.queryLeftNewDTO.yw_num,rw:.queryLeftNewDTO.rw_num,start_time:.queryLeftNewDTO.start_time,arrive_time:.queryLeftNewDTO.arrive_time}")
+res=$(curl -k -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36" "https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=$_date&leftTicketDTO.from_station=$_from&leftTicketDTO.to_station=$_to&purpose_codes=ADULT" 2>/dev/null | jq ".data[$_num]|{NO_:.queryLeftNewDTO.station_train_code,from:.queryLeftNewDTO.from_station_name,to:.queryLeftNewDTO.to_station_name,yw:.queryLeftNewDTO.yw_num,rw:.queryLeftNewDTO.rw_num,start_time:.queryLeftNewDTO.start_time,arrive_time:.queryLeftNewDTO.arrive_time,secretStr:.secretStr}")
 echo $res |jq '.'
 test -z "$res" && return
 echo $res |jq -e 'select((.yw != "无" and .yw != "--" and .yw !="null") or (.rw != "无" and .rw != "--" and .rw != "null"))'
-test "$?" -eq "0" && ok
+if test "$?" -eq "0"; then
+open $(echo $res |jq -e 'select((.yw != "无" and .yw != "--" and .yw !="null") or (.rw != "无" and .rw != "--" and .rw != "null")) |.secretStr')
+ok
+fi
 sleep .3
 }
 
