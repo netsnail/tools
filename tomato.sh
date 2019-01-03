c0e3fcf946135b4cb634e24e223203be9745775b
netsnail <flier.lh@gmail.com>
2019-01-03 09:47:30 +0800

    番茄钟

--- tomato.sh
+++ tomato.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-i=0
+i=1
 Xdialog --title "第 $i 个番茄" --center --yesno  '开始工作吗？' 8 40
 while test 0 -eq $?; do
   sleep 25m
-  i=$((i+1))
   Xdialog --title "第 $i 个番茄" --center --msgbox  '休息一会吧!' 8 40
   sleep 5m
   test 0 -eq $((i % 4)) && sleep 20m
+  i=$((i+1))
   Xdialog --title "第 $i 个番茄" --center --yesno  '继续工作吗？' 8 40
 done
