47ec209c1c92d212dd8cbaa0dd7b98e6f2a0ca85
netsnail <flier.lh@gmail.com>
2019-05-26 00:40:03 +0800

    语音服务

---
+++ tts.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var http = require('http'), 
+  fs = require('fs'), 
+  url = require('url');
+const { exec } = require('child_process');
+var tts = '~/bin/ekho.sh'
+
+http.createServer(function(req, rsp) {
+  var query = url.parse(req.url, true).query
+  var txt = query.txt
+  var p = query.p || 0
+  var a = query.a || 0
+  var s = query.s || 0
+  var filename = Math.random().toString(36).substr(2)
+  var file = '/tmp/'+filename
+
+  if (! txt) {
+    rsp.end('txt, Send request to TTS server \n'
+	  + 'p, Set delta pitch. Value range from -100 to 100 (percent) \n'
+	  + 'a, Set delta volume. Value range from -100 to 100 (percent) \n'
+	  + 's, Set delta speed. Value range from -50 to 300 (percent)\n'
+	)
+	return
+  }
+
+  var cmd = tts+' -togg -p'+p+' -a'+a+' -s'+s+' -o'+file+' '+txt
+  console.log(cmd)
+
+  exec(cmd).on('exit', (code) => {
+    rsp.writeHead(200, {
+        'Content-Type': 'application/ogg',
+        'Content-Length': fs.statSync(file).size
+    })
+
+    fs.createReadStream(file).pipe(rsp)
+	fs.unlink(file, () => {})
+  })
+
+}).listen(2000);
