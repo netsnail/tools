379c9c3cfd90643b97408c9a812de86c19139f1e
netsnail <flier.lh@gmail.com>
2022-10-15 22:30:42 +0800

    Create list.php

---
+++ list.php
@@ -0,0 +1,10 @@
+<?php //php -S 0:8000 list.php
+$path = trim($_SERVER['REQUEST_URI'],'/');
+$file = $_SERVER['DOCUMENT_ROOT'].'/'.$path;
+if (is_file($file)) return false;
+echo '<meta charset=utf8><pre><form method=post enctype=multipart/form-data><input name=f type=file /><input type=submit /></form>';
+if ($_FILES) 
+  move_uploaded_file($_FILES['f']['tmp_name'],$file.'/'.$_FILES['f']['name']);
+foreach (scandir($file) as $v) 
+  echo "<a href=/./$path/$v>$v".(is_dir($file.'/'.$v)?'/':'')."</a>\n";
+return true;
