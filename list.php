7e88899463805d6ca056cba26816da76eaaae5b3
netsnail <flier.lh@gmail.com>
2022-10-15 22:46:07 +0800

    Update list.php

--- list.php
+++ list.php
@@ -1,8 +1,8 @@
 <?php //php -S 0:8000 list.php
-$path = trim($_SERVER['REQUEST_URI'],'/');
-$file = $_SERVER['DOCUMENT_ROOT'].'/'.$path;
-if (is_file($file)) return false;
-echo '<meta charset=utf8><pre><form method=post enctype=multipart/form-data><input name=f type=file /><input type=submit /></form>';
-if ($_FILES) move_uploaded_file($_FILES['f']['tmp_name'],$file.'/'.$_FILES['f']['name']);
-foreach (scandir($file) as $v) echo "<a href=/./$path/$v>$v".(is_dir($file.'/'.$v)?'/':'')."</a>\n";
+$uri = trim($_SERVER['REQUEST_URI'],'/');
+$dir = $_SERVER['DOCUMENT_ROOT'].'/'.$uri;
+if (is_file($dir)) return false;
+echo "<meta charset=utf8>Index of $dir<form align=right method=post enctype=multipart/form-data><input name=f type=file /><input type=submit /></form><hr><pre>";
+if ($_FILES) move_uploaded_file($_FILES['f']['tmp_name'],$dir.'/'.$_FILES['f']['name']); 
+foreach (scandir($dir) as $v) echo "<a href=\"/./$uri/$v\">$v".(is_dir("$dir.'/'.$v")?'/':'')."</a>\n";
 return true;
